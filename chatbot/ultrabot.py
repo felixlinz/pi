c1638f4aa40d640747e6a39cb02cbad2a16fc774
felixlinz <felix.linz@icloud.com>
2023-06-08 13:24:18 +0100

    edited chatbot

--- chatbot/ultrabot.py
+++ chatbot/ultrabot.py
@@ -49,20 +49,25 @@ Please type one of these commands:
         return self.send_message(chatID, welcome_string)
 
 
-    def processingـincomingـmessages(self,text):
+    def processingـincomingـmessages(self):
+        message = self.dict_messages
+        text = message["body"]
+        chatID = message["from"]
         match = re.search(r"^(ferment)\b.*\b(set \w*)\b.*\-\s*(\d*)", text)
         if match:
             try:
                 if match.group(2) == "set temp":
-                    return f"temperature set to {int(match.group(3))} Degrees"
+                    return self.send_message(chatID, f"temperature set to {int(match.group(3))} Degrees")
                 elif match.group(2) == "set humidity":
-                    return f"humidity set to {int(match.group(3))} Percent Air Wetness"
+                    return self.send_message(chatID, f"humidity set to {int(match.group(3))} Percent Air Wetness")
                 elif match.group(2) == "set duration":
-                    return f"duration set to {int(match.group(3))} Hours"
+                    return self.send_message(chatID,f"duration set to {int(match.group(3))} Hours")
             except ValueError:
-                return f"specified numeric value for {match.group(2)} wasn't specified correctly\nUse Numeric values without any extra Symbols"
+                return self.send_message(chatID, f"specified numeric value for {match.group(2)} wasn't specified correctly\nUse Numeric values without any extra Symbols")
                 
         else: 
             attempt = r"^(ferment|fermentation)"
             if attempt:
                 return "Fermentation Chamber Please type one of these commands: \n*set temp- ?* \n*set temp- ?*\n*set humidity- ?*\n*set duration- ?*\n*turn off*"
+            else: 
+                return 'NoCommand'
