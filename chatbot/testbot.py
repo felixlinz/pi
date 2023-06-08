3c7785e32617ed5cd689ec55e55a63f197aa7bfe
felixlinz <felix.linz@icloud.com>
2023-06-08 12:54:40 +0100

    added new chatbot to the real bot

--- chatbot/testbot.py
+++ chatbot/testbot.py
@@ -1,19 +1,27 @@
 import re
 
 def main():
-    print(processingـincomingـmessages(input("Message: ")))
+    while True:
+        print(processingـincomingـmessages(input("Message: ")))
     
 
 def processingـincomingـmessages(text):
         match = re.search(r"^(ferment)\b.*\b(set \w*)\b.*\-\s*(\d*)", text)
         if match:
-            print(match.group(1))
-            print(match.group(2))
-            print(match.group(3))
-            if match.group(2) == "set temp":
-                return f"temperature set to {match.group(3)} Degrees"
+            try:
+                if match.group(2) == "set temp":
+                    return f"temperature set to {int(match.group(3))} Degrees"
+                elif match.group(2) == "set humidity":
+                    return f"humidity set to {int(match.group(3))} Percent Air Wetness"
+                elif match.group(2) == "set duration":
+                    return f"duration set to {int(match.group(3))} Hours"
+            except ValueError:
+                return f"specified numeric value for {match.group(2)} wasn't specified correctly\nUse Numeric values without any extra Symbols"
+                
         else: 
-            return 'incorrect input'
+            attempt = r"^(ferment|fermentation)"
+            if attempt:
+                return "Fermentation Chamber Please type one of these commands: \n*set temp- ?* \n*set temp- ?*\n*set humidity- ?*\n*set duration- ?*\n*turn off*"
             
 if __name__=="__main__":
     main()
--- chatbot/ultrabot.py
+++ chatbot/ultrabot.py
@@ -49,21 +49,20 @@ Please type one of these commands:
         return self.send_message(chatID, welcome_string)
 
 
-    def Processingـincomingـmessages(self):
-        match = re.search(r"^(ferment\w*)\b.*\b(set\w*)\b.*\-(.*)", self.dict_messages.lower())
+    def processingـincomingـmessages(self,text):
+        match = re.search(r"^(ferment)\b.*\b(set \w*)\b.*\-\s*(\d*)", text)
         if match:
-            if match.group(2) == "temp":
-                return self.temperature(chatID,text[1])
-            message = self.dict_messages
-            text = message['body'].split("-")
-            if self.message['fromMe']:
-                chatID  = message['from'] 
-                if text[0].lower() == 'set temp':
-                    return self.temperature(chatID,text[1])
-                elif text[0].lower() == 'humidity -':
-                    return self.humidity(chatID)
-                elif text[0].lower() == 'duration -':
-                    return self.duration(chatID)
-                else:
-                    return self.welcome(chatID, True)
-            else: return 'NoCommand'
+            try:
+                if match.group(2) == "set temp":
+                    return f"temperature set to {int(match.group(3))} Degrees"
+                elif match.group(2) == "set humidity":
+                    return f"humidity set to {int(match.group(3))} Percent Air Wetness"
+                elif match.group(2) == "set duration":
+                    return f"duration set to {int(match.group(3))} Hours"
+            except ValueError:
+                return f"specified numeric value for {match.group(2)} wasn't specified correctly\nUse Numeric values without any extra Symbols"
+                
+        else: 
+            attempt = r"^(ferment|fermentation)"
+            if attempt:
+                return "Fermentation Chamber Please type one of these commands: \n*set temp- ?* \n*set temp- ?*\n*set humidity- ?*\n*set duration- ?*\n*turn off*"
